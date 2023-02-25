98561c4f202f2fd03f9962d99b9822debe2a5523
Wing Fung Wu <ryanwu7275@gmail.com>
2023-02-24 16:43:01 -0800

    save changes

--- memSim.py
+++ memSim.py
@@ -1,138 +1,108 @@
 from optparse import OptionParser
-from collections import deque
 
-class VirtualMemory():
-    
-    def __init__(self, pages, algo, num_fram, addresses) -> None:
-        self.pages = pages
-        self.tlb = FIFO(pages)
-        self.page_table = None
-        #list of address from the file
-        self.addresses = addresses
-        
-        if algo == "OPT":
-            ...
-        elif algo == "LRU":
-            ...
-        else:
-            self.page_table = FIFO(num_fram)
-        
+class VirtualMemory:
+    def __init__(self, frames=256) -> None:
+        self.frames = frames
         
 
-    def lookup(self, algo):
-        if algo == 'FIFO':
-            self.page_table = FIFO(self.pages, 256)
-        # for each page:
-        #   if page in self.tlb.pages_in_memory():
-        #       physical addr = backing_store[frame number*256:(frame number+1)*256]
-        #   elif page in self.page_table.pages_in_memory():
-        #       physical addr = backing_store[frame number*256:(frame number+1)*256]
+class TLB:
+    def __init__(self, size=16):
+        self.size = size
+        self.entries = [None]*size
+        self.fifo_queue = []
+        self.hits = 0
+        self.misses = 0
 
-class FIFO():
-    
-    def __init__(self, pages, entries=16):
-        self.entries = entries
-        self.pages = pages
-        self.frames = deque(maxlen=entries)
-        self.pages_in_memory = dict()
-    
-    
-    def pop(self, key):
-        self.pages_in_memory.pop(key)
-        self.pages.remove(key)
-    
-    # return the page number if it's in the list
-    # else return None
-    def getVal(self, page_number):
-        for page in self.pages:
-            if page == page_number:
-                return page
-            return None
-    
-    # given the page_number and the value,
-    #if the page number in the set of pages_in_memory
-    # remove the page
-    # then append the page_number to pages
-    # add the value at the index page number to 
-    def setVal(self, page_number, newVal):        
-        if page_number in self.pages_in_memory:
-            self.pages.remove(page_number)
-        self.pages_in_memory[page_number] = newVal
-        self.pages.append(page_number)
+    def lookup(self, page_number):
+        if self.entries[page_number]:
+            self.hits += 1
+            return self.entries[page_number]
+        self.misses += 1
+        return None
+
+    def insert(self, page_number, frame_number): #FIFO implementation
+        if len(self.fifo_queue) == self.size:
+            oldest_page = self.fifo_queue.pop(0)
+            self.entries[oldest_page] = None
         
-        if(len(self.pages) > self.entries):
-            del self.pages_in_memory[self.pages.pop(0)]
-        
-    # i don't think we need this since we can do it
-    # for all of the algo in the virtual memory class
-    # as we loop through
+        self.fifo_queue.append(page_number)
+        self.entries[page_number] = frame_number
+
+    def hit_rate(self):
+        # Compute the hit rate as the number of hits divided by the total number of accesses
+        total = self.hits + self.misses
+        return self.hits/total if total > 0 else 0
+
+class PageTable:
+    def __init__(self, size=256) -> None:
+        self.size = size
+        self.entries = [None]*size
+        self.fifo_queue = []
+        self.page_faults = 0
+        self.lookups = 0
     
-    def count(self):
-        page_faults = 0
-        page_hits = 0
-        for page in self.pages:
-            if page not in self.pages_in_memory:
-                page_faults += 1
+    def lookup(self, page_number):
+        self.lookups += 1
+        if self.entries[page_number]:
+            return self.entries[page_number]
+        self.page_faults += 1
+        return None
+    
+    def insert(self, page_number, frame_number): #FIFO implementation
+        if len(self.fifo_queue) == self.size:
+            oldest_page = self.fifo_queue.pop(0)
+            self.entries[oldest_page] = None
+        
+        self.fifo_queue.append(page_number)
+        self.entries[page_number] = frame_number
+    
+    def get_num_page_faults(self):
+        return self.page_faults
+    
+    def get_page_fault_rate(self):
+        return self.page_faults/self.lookups if self.lookups > 0 else 0
 
-                if len(self.frames) == self.entries:
-                    oldest_page = self.frames.popleft()
-                    self.pages_in_memory.remove(oldest_page)
-                
-                self.frames.append(page)
-                self.pages_in_memory.add(page)
-            else:
-                page_hits += 1
-        return page_faults, page_hits
-
-    def stat(self):
-        page_faults, page_hits = self.count()
-        total = page_faults + page_hits
-        return page_faults/total*100, page_hits/total*100
-
-def read_backing_store(frame):
-    with open("BACKING_STORE.bin", "rb") as file:
-        file.seek(256*frame)
-        return file.read(256)
-
-
-def cal_offset(offset):
-    if offset > 127:
-        return offset - 256
-    return offset
 # @param addr: 32-bit int representing a logical addr
 # @return page number, offset
 def mask_logical_addr(addr):
-    return (addr >> 8), (addr & 0xFF)
+    return (addr >> 8), (addr & 0xff)
+        
+def main():
+    
+    page_table = PageTable()
+    virtual_addresses = [16916, 62493, 30198, 53683, 40185, 28781, 24462, 48399, 64815, 18295]
+    
+    virtual_address = virtual_addresses[0]
+    page_number, offset = mask_logical_addr(virtual_address)
+    frame_number = page_table.lookup(page_number)
+    if not frame_number:
+        frame_number = len(page_table.fifo_queue)
+        page_table.insert(page_number, frame_number)
+    physical_address = (frame_number << 8) | offset
 
+    print(f"Virtual address: 0x{virtual_address:02X}")
+    print(f"Page number: {page_number}")
+    print(f"Offset: {offset}")
+    print(f"Frame number: {frame_number}")
+    print(f"Physical address: 0x{physical_address:02X}")
+    print(f"Page fault rate: {page_table.get_page_fault_rate():.2%}")
+
+'''
 def main():
     parser = OptionParser()
-    #parser.add_option("-f","--frames", default=256, help="memSim frames to use: an integer <= 256 and > 0", action="store", type="int", dest="frames")
-    #parser.add_option("-pra","--PRA", default="FIFO", help="memSim page replacement algorithms to use: FIFO, LRU, OPT", action="store", type="string", dest="pra")
+    parser.add_option("-f","--frames", default=256, help="memSim frames to use: an integer <= 256 and > 0", action="store", type="int", dest="frames")
+    parser.add_option("-p","--PRA", default="FIFO", help="memSim page replacement algorithms to use: FIFO, LRU, OPT", action="store", type="string", dest="pra")
     (option, args) = parser.parse_args()
     if not len(args):
         print("No Input Detected")
         return
     
-    pageFrame, offset =mask_logical_addr(16916)
+    #pageFrame, offset =mask_logical_addr(16916)
     
     #frames = option.frames
     #if frames < 0 or frames > 256:
         #frames = 256
-    '''
-    try:
-        with open(args[0], 'r') as file:
-            addrs = file.read()
-            addrs = addrs.split()
-            temp = []
-            for addr in addrs:
-                temp.append(mask_logical_addr(int(addr)))
-        with open("test.txt", 'w') as out:
-            for i in range(len(temp)):
-                out.write(f'{temp[i][0]}, {i}\n')
-    except FileNotFoundError:
-        print("No Input File Detected")
-        return
-    '''
+'''
     
     
 if __name__ == '__main__':
